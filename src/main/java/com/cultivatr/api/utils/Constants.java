e50d42c5f1fd9509849b66c7fd00d557442f3a7a
lalaura.lourenco <laura.lourenco@thoughtworks.com>
2020-06-05 14:48:44 -0300

    [Laura|Matheus] Fixes Production frontend URL

--- src/main/java/com/cultivatr/api/utils/Constants.java
+++ src/main/java/com/cultivatr/api/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.cultivatr.api.utils;
 
 public class Constants {
-    public static String frontProdURL = "https://mycultivatr.herokuapp.com/";
+    public static String frontProdURL = "https://mycultivatr.herokuapp.com";
     public static String frontDevURL = "http://localhost:3000";
 }

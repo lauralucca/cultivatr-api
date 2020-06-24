c3ad54d3303fe92bf51b5fb3458d3aa22ec580e3
lalaura.lourenco <laura.lourenco@thoughtworks.com>
2020-06-24 14:42:39 -0300

    [Laura|Matheus] Clears database after running integration test

--- src/main/java/com/cultivatr/api/controller/UserSignUpController.java
+++ src/main/java/com/cultivatr/api/controller/UserSignUpController.java
@@ -2,11 +2,10 @@ package com.cultivatr.api.controller;
 
 import com.cultivatr.api.model.User;
 import com.cultivatr.api.service.UserSignUpService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserSignUpController {
@@ -23,4 +22,13 @@ public class UserSignUpController {
         this.userSignUpService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+
+    @GetMapping("/user")
+    public String getUserByEmail(@RequestParam(value = "email") String email) throws Exception {
+        User user = this.userSignUpService.getUserByEmail(email);
+        ObjectMapper objectMapper = new ObjectMapper();
+        return  objectMapper.writeValueAsString(user);
+    }
+
 }
--- src/test/java/com/cultivatr/api/integration/UserSignUpIntegrationTest.java
+++ src/test/java/com/cultivatr/api/integration/UserSignUpIntegrationTest.java
@@ -1,14 +1,20 @@
 package com.cultivatr.api.integration;
 
+import com.cultivatr.api.PostgresConnection;
 import com.cultivatr.api.model.User;
 import com.cultivatr.api.service.UserSignUpService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,19 +22,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(locations = "/application-test.properties")
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserSignUpIntegrationTest {
+    @Value("${db.host}")
+    String host;
+    @Value("${db.port}")
+    String port;
+    @Value("${db.name}")
+    String db_name;
+    @Value("${db.username}")
+    String username;
+    @Value("${db.password}")
+    String password;
+    @Value("${db.driver}")
+    String driver;
+
+    User expectedUser;
+    String name;
+    String email;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private UserSignUpService userSignUpService;
 
+    @BeforeAll
+    void initializeTestData() {
+        this.name = "Lucas";
+        this.email = "lucas@cultivatr.com" + Math.random();
+    }
+
     @Test
     void createsUser() throws Exception {
-        String name = "Lucas";
-        String email = "lucas@cultivatr.com" + Math.random();
-        String userAsJson = "{ \"name\": \"" + name + "\", " +
-                "\"email\": \"" + email + "\"" + "}";
+
+        String userAsJson = "{ \"name\": \"" + this.name + "\", " +
+                "\"email\": \"" + this.email + "\"" + "}";
 
         mockMvc.perform(
                 post("/user")
@@ -36,10 +65,17 @@ public class UserSignUpIntegrationTest {
                         .content(userAsJson))
                 .andExpect(status().isCreated());
 
-        User expectedUser = userSignUpService.getUserByEmail(email);
+        User expectedUser = userSignUpService.getUserByEmail(this.email);
 
-        assertThat(expectedUser.getName()).isEqualTo(name);
-        assertThat(expectedUser.getEmail()).isEqualTo(email);
+        assertThat(expectedUser.getName()).isEqualTo(this.name);
+        assertThat(expectedUser.getEmail()).isEqualTo(this.email);
 
+    }
+
+    @AfterAll
+    void clearDataBase() throws Exception {
+
+        PostgresConnection DBConnection = new PostgresConnection(host, port, db_name, username, password, driver);
+        DBConnection.executeCommand("DELETE from user_account");
     }
 }
